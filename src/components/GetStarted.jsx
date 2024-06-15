0efeedba2573d450ebbf0630592ba40b65495db4
Anuj Yadav <anujyadav-lab>
2024-06-15 15:56:03 +0530

    done with first page

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import Logo from "../Img/osumare.png";
+import HeaderImg from "../Img/Headerimage.png";
+import Cuate from '../Img/cuate.png'
+
 import backgroundImg from '../Img/Bg.png'
 const GetStarted = () => {
   return (
-    <div className=" " style={{backgroundImage:`url(${backgroundImg})`}}>
+    <div className="bg-cover bg-center min-h-screen " style={{backgroundImage:`url(${backgroundImg})`}}>
       <div className=" bg-slate-50 p-4 flex items-center justify-between w-ful">
         <img className=" w-44 h-44 cursor-pointer" src={Logo} alt="Logo" />
         <button className=" font-bold border-2 rounded-3xl   px-[58px] py-[10px] ">Contact Us</button>
@@ -21,6 +24,30 @@ const GetStarted = () => {
 
 
       </div>
+      <div>
+      <img className=" w-4/12 h-96 mx-auto block mt-16" src={HeaderImg} alt="Logo" />
+
+      <h1 className=" mt-36 font-bold  text-2xl flex justify-center">Real Estate-Focused Digital Mastery</h1>
+
+      <div className="mt-36 flex items-center justify-center">
+  <img className="w-96 ml-72" src={Cuate} alt="img" />
+  <div className="ml-8">
+    <h1 className="font-bold text-2xl">
+      Unlock the Potential of Digital Real Estate Excellence
+    </h1>
+    <p className="mt-2">
+      At Osumare, we understand that the real estate landscape demands a digital
+      presence that aligns with the intricacies of property marketing. Our range
+      of specialized services is meticulously designed to catapult your brand's
+      success in the ever-evolving digital property market.
+    </p>
+    <button className=" font-semibold border-2 rounded-3xl   px-[60px] py-[10px] bg-blue-500 text-lg  text-white mt-8  ">Get Started</button>
+
+  </div>
+</div>
+
+      </div>
+      
     </div> 
   );
 };
